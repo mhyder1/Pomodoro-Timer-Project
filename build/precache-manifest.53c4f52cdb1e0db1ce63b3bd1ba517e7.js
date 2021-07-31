2f7cc07a84c4f37eaef0ace9843ca519c2a5e775
unknown <hyder1@gmail.com>
2021-07-31 00:15:10 -0400

    build

---
+++ build/precache-manifest.53c4f52cdb1e0db1ce63b3bd1ba517e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d082418338138770e69b30792cd9472c",
+    "url": "/Pomodoro-Timer-Project/index.html"
+  },
+  {
+    "revision": "e2c005ac777150dc6f2a",
+    "url": "/Pomodoro-Timer-Project/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/Pomodoro-Timer-Project/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pomodoro-Timer-Project/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c005ac777150dc6f2a",
+    "url": "/Pomodoro-Timer-Project/static/js/main.1f177b4c.chunk.js"
+  },
+  {
+    "revision": "21229d2c6252c0abef24",
+    "url": "/Pomodoro-Timer-Project/static/js/runtime-main.5cffff8a.js"
+  }
+]);
